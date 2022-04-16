aa32bfa7fac14d6977d3f256142b19a806fac4d8
Vladislav Melnichuk <vs.melnichuk09@gmail.com>
2022-04-16 05:36:06 +0300

    final
    final

---
+++ lib_double/validation.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+bool isValidEmail(std::string);
+bool isValidUsername(std::string);
+bool isValidPassword(std::string);
+bool isValidName(std::string);
+bool isValidSurname(std::string);
+bool isValidDescription(std::string);
+bool isValidProjectDescription(std::string);
+bool isValidRequestDescription(std::string);
+bool isValidTeamName(std::string);
+bool isValidProjectName(std::string);
+bool isValidProjectNames(std::vector<std::string>);
+bool isValidPostTags(std::vector<std::string>);
+bool isValidTeammates(std::vector<std::string>);
+bool isValidDiversity(int);
+bool isValidQuestion(std::string);
+bool isValidProjects(std::vector<NotificationProjectData>);
+bool isValidSearchData(std::string);
+bool isValidPostData(MainPostData);
+bool isValidAgeFrom(int);
+bool isValidAgeTo(int);
+bool isValidLanguage(std::string);
