e368b68550d53733788e155f2febc89791644874
alexey190900 <mte1909@mail.ru>
2022-05-01 19:53:35 +0300

    async client

--- libs_testing/client.cpp
+++ libs_testing/client.cpp
@@ -9,102 +9,186 @@
 
 //------------------------------------------------------------------------------
 //
-// Example: HTTP client, synchronous
+// Example: HTTP client, asynchronous
 //
 //------------------------------------------------------------------------------
-
-//[example_http_client
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
 #include <cstdlib>
+#include <functional>
 #include <iostream>
+#include <memory>
 #include <string>
 
-namespace beast = boost::beast;     // from <boost/beast.hpp>
-namespace http = beast::http;       // from <boost/beast/http.hpp>
-namespace net = boost::asio;        // from <boost/asio.hpp>
-using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+//------------------------------------------------------------------------------
+
+// Report a failure
+void
+fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
 
 // Performs an HTTP GET and prints the response
-int main(int argc, char** argv)
+class session : public std::enable_shared_from_this<session>
 {
-    try
+    tcp::resolver resolver_;
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_; // (Must persist between reads)
+    http::request<http::empty_body> req_;
+    http::response<http::string_body> res_;
+
+public:
+    // Objects are constructed with a strand to
+    // ensure that handlers do not execute concurrently.
+    explicit
+    session(net::io_context& ioc)
+        : resolver_(net::make_strand(ioc))
+        , stream_(net::make_strand(ioc))
     {
-        // Check command line arguments.
-        if(argc != 3 && argc != 4)
-        {
-            std::cerr <<
-                "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-                "Example:\n" <<
-                "    http-client-sync www.example.com 80\n" <<
-                "    http-client-sync www.example.com 80 1.0\n";
-            return EXIT_FAILURE;
-        }
-        auto const host = argv[1];
-        auto const port = argv[2];
+    }
 
-        std::cout << "Enter target\n" << "";
-
-        std::string target;
-        std::cin >> target;
-
-        int version = argc == 5 && !std::strcmp("1.0", argv[3]) ? 10 : 11;
-
-        // The io_context is required for all I/O
-        net::io_context ioc;
-
-        // These objects perform our I/O
-        tcp::resolver resolver(ioc);
-        beast::tcp_stream stream(ioc);
+    // Start the asynchronous operation
+    void
+    run(
+        std::string host,
+        std::string port,
+        std::string target,
+        int version)
+    {
+        // Set up an HTTP GET request message
+        req_.version(version);
+        req_.method(http::verb::get);
+        req_.target(target);
+        req_.set(http::field::host, host);
+        req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
         // Look up the domain name
-        auto const results = resolver.resolve(host, port);
+        resolver_.async_resolve(
+            host,
+            port,
+            beast::bind_front_handler(
+                &session::on_resolve,
+                shared_from_this()));
+    }
+
+    void
+    on_resolve(
+        beast::error_code ec,
+        tcp::resolver::results_type results)
+    {
+        if(ec)
+            return fail(ec, "resolve");
+
+        // Set a timeout on the operation
+        stream_.expires_after(std::chrono::seconds(30));
 
         // Make the connection on the IP address we get from a lookup
-        stream.connect(results);
+        stream_.async_connect(
+            results,
+            beast::bind_front_handler(
+                &session::on_connect,
+                shared_from_this()));
+    }
 
-        // Set up an HTTP GET request message
-        http::request<http::string_body> req{http::verb::get, target.data(), version};
-        req.set(http::field::host, host);
-        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    void
+    on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type)
+    {
+        if(ec)
+            return fail(ec, "connect");
+
+        // Set a timeout on the operation
+        stream_.expires_after(std::chrono::seconds(30));
 
         // Send the HTTP request to the remote host
-        http::write(stream, req);
+        http::async_write(stream_, req_,
+            beast::bind_front_handler(
+                &session::on_write,
+                shared_from_this()));
+    }
 
-        // This buffer is used for reading and must be persisted
-        beast::flat_buffer buffer;
+    void
+    on_write(
+        beast::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
 
-        // Declare a container to hold the response
-        http::response<http::dynamic_body> res;
-
+        if(ec)
+            return fail(ec, "write");
+        
         // Receive the HTTP response
-        http::read(stream, buffer, res);
+        http::async_read(stream_, buffer_, res_,
+            beast::bind_front_handler(
+                &session::on_read,
+                shared_from_this()));
+    }
+
+    void
+    on_read(
+        beast::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        if(ec)
+            return fail(ec, "read");
 
         // Write the message to standard out
-        std::cout << res << std::endl;
+        std::cout << res_ << std::endl;
 
         // Gracefully close the socket
-        beast::error_code ec;
-        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+        stream_.socket().shutdown(tcp::socket::shutdown_both, ec);
 
-        // not_connected happens sometimes
-        // so don't bother reporting it.
-        //
+        // not_connected happens sometimes so don't bother reporting it.
         if(ec && ec != beast::errc::not_connected)
-            throw beast::system_error{ec};
+            return fail(ec, "shutdown");
 
         // If we get here then the connection is closed gracefully
     }
-    catch(std::exception const& e)
+};
+
+//------------------------------------------------------------------------------
+
+int main(int argc, char** argv)
+{
+    // Check command line arguments.
+    if(argc != 3 && argc != 4)
     {
-        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr <<
+            "Usage: http-client-async <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
+            "Example:\n" <<
+            "    http-client-async www.example.com 80 /\n" <<
+            "    http-client-async www.example.com 80 / 1.0\n";
         return EXIT_FAILURE;
     }
+    std::string host = argv[1];
+    std::string port = argv[2];
+    // auto const target = argv[3];
+
+    std::cout << "Enter target\n" << "";
+    std::string target;
+    std::cin >> target;
+
+    int version = argc == 5 && !std::strcmp("1.0", argv[3]) ? 10 : 11;
+
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // Launch the asynchronous operation
+    std::make_shared<session>(ioc)->run(host, port, target, version);
+
+    // Run the I/O service. The call will return when
+    // the get operation is complete.
+    ioc.run();
+
     return EXIT_SUCCESS;
 }
-
-//]
